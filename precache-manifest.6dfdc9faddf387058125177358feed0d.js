f32ad8609a130d234c922387d1101cc41e84b984
marcuasc <marcus6623@hotmail.com>
2019-11-04 23:03:59 +0100

    Updates

---
+++ precache-manifest.6dfdc9faddf387058125177358feed0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd80d14eb450c64d0cc822d23568ec99",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "893206e4f1d7a25ea537",
+    "url": "/react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "579d0e578d14b235c4af",
+    "url": "/react/static/js/2.ce1dfa14.chunk.js"
+  },
+  {
+    "revision": "893206e4f1d7a25ea537",
+    "url": "/react/static/js/main.cdb8f1b6.chunk.js"
+  },
+  {
+    "revision": "dde4f3e26f6c066c1ee5",
+    "url": "/react/static/js/runtime-main.00bbd472.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react/static/media/logo.25bf045c.svg"
+  }
+]);
