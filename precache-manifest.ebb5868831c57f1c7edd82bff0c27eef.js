e20515230c5370f766d5e8b5d9f854119c4ff934
marcuasc <marcus6623@hotmail.com>
2019-11-04 23:43:33 +0100

    Updates

---
+++ precache-manifest.ebb5868831c57f1c7edd82bff0c27eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57a51feac24b7b56074077c989c529f",
+    "url": "bymarcus.me/index.html"
+  },
+  {
+    "revision": "e32f5c08004754114e05",
+    "url": "bymarcus.me/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "579d0e578d14b235c4af",
+    "url": "bymarcus.me/static/js/2.ce1dfa14.chunk.js"
+  },
+  {
+    "revision": "e32f5c08004754114e05",
+    "url": "bymarcus.me/static/js/main.9e1c7b86.chunk.js"
+  },
+  {
+    "revision": "eb2b0e0a8dae05871cb0",
+    "url": "bymarcus.me/static/js/runtime-main.f3cdafb3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "bymarcus.me/static/media/logo.25bf045c.svg"
+  }
+]);
