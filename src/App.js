f9d2ce8b5a1459480fece0dbd5f68469c0a229c2
marcuasc <marcus6623@hotmail.com>
2019-11-04 23:51:00 +0100

    Updated paragrap on index

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        This site is under development.
+        By Marcus Schroder
         </p>
         <a
           className="App-link"
